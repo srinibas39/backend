2f2e938ec5e69dde22bf15875ebae39b4c0a4cc9
srinibas <srinibaskhuntia39@gmail.com>
2022-06-25 12:46:59 +0530

    implemented express

---
+++ app.js
@@ -0,0 +1,31 @@
+
+const express = require("express");
+const app = express();
+
+app.listen(3000);
+
+
+app.get('/', (req, res) => {
+    res.sendFile("./pages/index.html", { root: __dirname })
+});
+
+app.get('/amazon', (req, res) => {
+    res.sendFile("./pages/Amazon.html", { root: __dirname });
+});
+
+app.get('/flipkart', (req, res) => {
+    res.sendFile("./pages/Flipkart.html", { root: __dirname });
+})
+
+
+// Redirect
+
+app.get("/amazonaa", (req, res) => {
+    res.redirect("/amazon")
+})
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).sendFile("./pages/404.html", { root: __dirname })
+})
+
