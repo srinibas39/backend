cd2181873ca55142b2af08ee0a61e034366590d1
srinibas <srinibaskhuntia39@gmail.com>
2022-06-25 15:10:06 +0530

    query and parameter implemented

--- method.js
+++ method.js
@@ -5,14 +5,28 @@ const app = express();
 app.listen(3000);
 app.use(express.json())
 
-let users = {};
+let users = [
+    {
+        id: "1",
+        name: "ram"
+    },
+    {
+        id: "2",
+        name: "sam"
+    },
+    {
+        id: "3",
+        name: "bam"
+    }
+];
 // get
 app.get("/users", (req, res) => {
     res.send(users);
 })
 
+// post
 app.post("/users", (req, res) => {
-    users={...req.body};
+    users = { ...req.body };
     res.send(users);
 })
 
@@ -24,8 +38,22 @@ app.patch("/users", (req, res) => {
 })
 
 // delete
-app.delete("/users", (req, res) => {
+app.delete("/users/:id", (req, res) => {
     users = {};
     res.send(users);
 })
 
+
+// parameter
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params.id
+    const user = users.filter((el) => el.id === id)
+    res.send({ ...user })
+})
+
+// Query
+
+// app.get("/users", (req, res) => {
+//     console.log(req.query)
+// })
